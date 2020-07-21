aa702e6d96845bb13765d34dbfbb6a2b5b29b318
Laojieduo <xzjxzj@stu.xjtu.edu.cn>
2020-07-21 14:03:55 +0800

    更新了一个测试类 usertest正在写

---
+++ src/test/java/com/jiading/service/UserTest.java
@@ -0,0 +1,33 @@
+package com.jiading.service;
+
+
+import com.jiading.model.Post;
+import com.jiading.model.Reply;
+import com.jiading.util.DateUtil;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @program: bbs
+ *
+ * @description: 测试user类型
+ *
+ * @author: Laojieduo
+ *
+ * @create: 2020-07-21 11:24
+ **/
+
+public class UserTest extends BaseTest {
+
+    @Autowired
+    UserService
+
+
+
+
+
+}
